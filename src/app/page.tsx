c983086b6cc72ab8865a221be840e869ee05f861
Tex Tang <tex_tang@outlook.com>
2023-08-15 20:44:37 +0800

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,11 @@ export default function Home() {
       <LoginForm />
 
       <p className="mt-4">
+        Login with your NUS EVS credential to view your EVS credit. Your credit will be updated every 4 hours and you
+        may track your EVS credit history.
+      </p>
+
+      <p className="mt-4">
         Disclaimer: This is a personal project and is not affiliated with NUS, NUS EVS, or any other. Your NUS EVS
         credential will be stored in our database. We will not use it for any other purpose other than update your EVS
         credit.
