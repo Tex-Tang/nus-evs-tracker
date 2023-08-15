4659af2dc42af743cd3cd59a6b6a12764f9c4e42
Tex Tang <tex_tang@outlook.com>
2023-08-15 20:39:00 +0800

    Add disclaimer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,12 @@ export default function Home() {
     <main className="max-w-2xl mx-auto my-8">
       <h1 className="mb-4">NUS EVS Tracker</h1>
       <LoginForm />
+
+      <p className="mt-4">
+        Disclaimer: This is a personal project and is not affiliated with NUS, NUS EVS, or any other. Your NUS EVS
+        credential will be stored in our database. We will not use it for any other purpose other than update your EVS
+        credit.
+      </p>
     </main>
   );
 }
