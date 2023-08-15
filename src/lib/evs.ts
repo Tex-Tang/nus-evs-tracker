4bd22e09fb692b12f7de7b8860df8a55d6d4e7ea
Tex Tang <tex_tang@outlook.com>
2023-08-15 22:02:18 +0800

    Fix parse with timezone function

--- src/lib/evs.ts
+++ src/lib/evs.ts
@@ -13,8 +13,8 @@ export const meterSchema = zod.object({
 
 export type Meter = Zod.infer<typeof meterSchema>;
 
-function parseWithSingaporeTimezone(dateStr: string) {
-  const parsedDate = parse(dateStr, "dd/MM/yyyy HH:mm", new Date());
+function parseWithSingaporeTimezone(dateStr: string, format: string) {
+  const parsedDate = parse(dateStr, format, new Date());
   return zonedTimeToUtc(parsedDate, "Asia/Singapore");
 }
 
@@ -56,7 +56,7 @@ export async function listLatestTransactions(cookie: string) {
   const data = [];
   for (const row of rows) {
     data.push({
-      timestamp: parseWithSingaporeTimezone(row.children[1].textContent ?? ""),
+      timestamp: parseWithSingaporeTimezone(row.children[1].textContent ?? "", "dd/MM/yyyy HH:mm"),
       amount: Number(row.children[2].textContent?.trim() ?? 0),
     });
   }
@@ -88,11 +88,15 @@ export async function getMeterCredit(cookie: string) {
   );
   const lastRecordedCredit = Number(lastRecordedCreditEl?.textContent?.replace("S$", "").trim());
 
+  console.log;
   const lastRecordedTimestampEl = document.querySelector(
     "#frmViewMeterCredit > table > tbody > tr:nth-child(9) > td:nth-child(2) > font"
   );
 
-  const lastRecordedTimestamp = parseWithSingaporeTimezone(lastRecordedTimestampEl?.textContent ?? "");
+  const lastRecordedTimestamp = parseWithSingaporeTimezone(
+    lastRecordedTimestampEl?.textContent ?? "",
+    "dd/MM/yyyy HH:mm:ss"
+  );
 
   return {
     meterId,
